edb19cefd1d113d366c6a1424668725477cf85b5
Erich Richter <erich.richter@gmail.com>
2020-01-24 10:10:03 -0800

    add javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,28 @@
 $(document).ready(function(){
 
+
+var userInput = 24;
+var userNumberRange =[];
+var numberOne = "Beep!";
+var numberTwo = "Boop!";
+var numberThree = "I'm sorry dave, I can't do that."
+
+
+for (i = 0 ; i <= userInput ; i++){
+
+  userNumberRange.push(i);
+  
+}
+
+
+userNumberRange.forEach(function(item, i) { cd desktop
+  if (item === 1) userNumberRange[i] = "Beep!"; });
+
+
+console.log(userNumberRange)
+
+
+
+
+
 });
