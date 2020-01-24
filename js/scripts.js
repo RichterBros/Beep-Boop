8f36e17d8cac557bde4bb80f3f10c756be2ea158
Erich Richter <erich.richter@gmail.com>
2020-01-24 15:49:07 -0800

    add javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,67 @@
 $(document).ready(function(){
 
 
-var userInput = 24;
-var userNumberRange =[];
-var numberOne = "Beep!";
-var numberTwo = "Boop!";
-var numberThree = "I'm sorry dave, I can't do that."
+// var userInput = 24;
+// var userNumberRange =[];
+// var numberOne = "Beep!";
+// var numberTwo = "Boop!";
+// var numberThree = "I'm sorry dave, I can't do that."
 
 
-for (i = 0 ; i <= userInput ; i++){
+// for (i = 0 ; i <= userInput ; i++){
 
-  userNumberRange.push(i);
+//   userNumberRange.push(i);
   
+// }
+
+// itemCopy.forEach(function(word){
+//   if (word.length ===2){
+//     test.push(word);
+//   }
+
+
+// });
+var userInput = 25
+var userNums = []
+var userNum = [];
+var newTest = []
+var test =[]
+var res = []
+var separateNums =[]
+for (i =0 ; i < userInput ; i++){
+  userNums.push(i);
 }
 
 
-userNumberRange.forEach(function(item, i) { cd desktop
-  if (item === 1) userNumberRange[i] = "Beep!"; });
+    userNums.forEach(function(num){
+      
+      
+        
+        test.push(num);
+        stringNums = test.toString();
+      
+        
+        
+      //newTest.push(res)
+    
+      
+      
+      });
+      var res = stringNums.replace("1", "Beep!");
+    
+      console.log(res)
+
+   
+  
+    //$(this).index()
 
 
-console.log(userNumberRange)
+
+
+
+
+
+
 
 
 
