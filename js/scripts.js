98b6412c2f7e1457a8ee7e3ecc5903c680f9285b
Erich Richter <erich.richter@gmail.com>
2020-01-24 18:17:55 -0800

    funcionality to get greater number of first two numbers in array

--- js/scripts.js
+++ js/scripts.js
@@ -9,29 +9,60 @@ var newTest = []
 var test =[]
 var res = []
 var separateNums =[]
-for (i =0 ; i < userInput ; i++){
+var output = []
+var numPair1 =[]
+var numPair2 =[]
+//this grabs the user inputted number and count up to the user number from 10
+var stringNumSolid =[]
+for (i =10; i < userInput ; i++){
+  
+  
+  
+  
   userNums.push(i);
 }
 
+console.log(userNums)
+    
 
-    userNums.forEach(function(num){
+//this takes the numbers and converts them to one string:
+
+userNums.forEach(function(num){
       
+     test.push(num);
+        stringNums = test.toString()
       
-        
-        test.push(num);
-        stringNums = test.toString();
-      
-        
-        
+        stringNumSolid = stringNums.split(',').join('');
+        //result = Math.max([i], [i]);
      
-    
-     
+    });
       
-      });
+      console.log(stringNumSolid)
+      //this takes the string and separates the numbers:
+      var number = stringNumSolid,
+      outputs = [],
+      sNumber = number.toString();
+  
+  for (var i = 0, len = sNumber.length; i < len; i += 1) {
+      output.push(+sNumber.charAt(i));
+  }
+  
+     //here are the individual numbers separated: 
+  console.log(output)
       
-      console.log(stringNums)
-      //var res = stringNums.replace(/1/g, "Beep!");
-    
+
+//here is where we get the first two numbers:
+
+  numPair1 = output.shift();
+
+console.log(numPair1)
+ 
+numPair2 = output.shift();
+
+console.log(numPair2)
+result = Math.max(numPair1, numPair2);
+      console.log(result)
+   //this will be used to replace the numbers with words: 
       var mapObj = {
         1:"beep",
         2:"boop",
@@ -44,17 +75,12 @@ for (i =0 ; i < userInput ; i++){
      
      
      
-      //console.log(res)
+      console.log(res)
 
    
-  // var i = 5;
-  // var x = 6
-  //  var result = 0 
-   
-  //      result = Math.max(5, 6);
-     
-  //      console.log(result)
-      
+
+   //this will be used to find the greater of the two numbers once isolated
+  //result = Math.max(5, 6);
 
 
 
