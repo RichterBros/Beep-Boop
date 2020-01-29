be2ceba743ead888921d91b972c2d8d5ce2132ef
Student <example@example.com>
2020-01-29 13:14:28 -0800

    separate business from front end

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,34 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  $(".btn").click(function (event) {
+    event.preventDefault();
+    var printString = test()
+    $("#output").text(printString);
 
-$(".btn").click(function() {
-    
-event.preventDefault();  
-    
-var userNums = []
-var strList = []
-
-var userInput = parseInt($("input#number").val()) +1;
-
-for (i =0; i < userInput ; i++){
-  userNums.push(i);
-  userStr = userNums[i].toString();
-  strList.push(userStr)
-}
-
-for(c=0; c<userInput; c++ ){
-  x=strList[c]
-  if(x.includes("3") ){
-    strList[c]="I'm sorry, Dave. I'm afraid I can't do that."
-    }
-    else if(x.includes("2") ){
-    strList[c]="boop"
-    }
-    else if(x.includes("1") ){
-    strList[c]="beep"
-    }
-  } 
-  $("#output").text(strList);
   });
 });
+
+function test() {
+  var userNums = []
+  var strList = []
+
+  var userInput = parseInt($("input#number").val()) + 1;
+
+  for (i = 0; i < userInput; i++) {
+    userNums.push(i);
+    userStr = userNums[i].toString();
+    strList.push(userStr)
+  }
+  for (c = 0; c < userInput; c++) {
+    x = strList[c]
+    if (x.includes("3")) {
+      strList[c] = "I'm sorry, Dave. I'm afraid I can't do that."
+    }
+    else if (x.includes("2")) {
+      strList[c] = "boop"
+    }
+    else if (x.includes("1")) {
+      strList[c] = "beep"
+    }
+  }
+  return strList;
+}
